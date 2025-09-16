75a164215943a10d8d17d98154b356656acc9218
yguira-hub <y_guira@inptic.edu.dz>
2025-09-16 18:42:07 +0800

    Ajout de la classe principale pour Spring Boot

---
+++ src/main/java/com/yguira/RegisterAppApplication.java
@@ -0,0 +1,11 @@
+package com.yguira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterAppApplication.class, args);
+    }
+}
