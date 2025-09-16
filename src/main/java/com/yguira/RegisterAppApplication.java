66854b64d1b1a9b294462ee5fb5a6e5e8cd8b0de
yguira-hub <y_guira@inptic.edu.dz>
2025-09-16 23:30:00 +0100

    Update RegisterAppApplication.java

--- src/main/java/com/yguira/RegisterAppApplication.java
+++ src/main/java/com/yguira/RegisterAppApplication.java
@@ -2,10 +2,19 @@ package com.yguira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RegisterAppApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(RegisterAppApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Register App! This is working!";
     }
 }
