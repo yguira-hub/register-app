03a8c120ecfbf70ffa0fc5aa553f814ec0297c86
yguira-hub <y_guira@inptic.edu.dz>
2025-09-20 16:48:28 +0100

    corr RegisterAppApplicationTest.java

--- src/test/java/com/yguira/RegisterAppApplicationTest.java
+++ src/test/java/com/yguira/RegisterAppApplicationTest.java
@@ -1,13 +1,14 @@
-package com.yguira.registerapp;
+package com.yguira;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(classes = RegisterAppApplication.class)
-class RegisterAppApplicationTests {
+@SpringBootTest
+class RegisterAppApplicationTest {
 
     @Test
     void contextLoads() {
+        assertThat(true).isTrue(); // Exemple de test simple
     }
-
 }
