d634743a5086529f662499d66dfb12f54972bc57
yguira-hub <y_guira@inptic.edu.dz>
2025-09-28 20:57:57 +0100

    Create AppTest.java

---
+++ src/test/java/com/yguira/registerapp/AppTest.java
@@ -0,0 +1,14 @@
+package com.yguira.registerapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void testAddition() {
+        // Exemple simple : vérifier que 2 + 3 = 5
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
